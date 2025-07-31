cf83bf0d15c4ec7d21bb805fdd82ed02e863e010
pc-rumah <wow23787@gmail.com>
2025-07-31 12:59:51 +0700

    ganti build css,js dan fix responsive gambar

--- resources/views/partdash/sidebar.blade.php
+++ resources/views/partdash/sidebar.blade.php
@@ -2,8 +2,8 @@
     <!-- Sidebar scroll-->
     <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
-            <a href="./index.html" class="text-nowrap logo-img">
-                <img src="{{ asset('dash/assets/images/logos/dark-logo.svg') }}" width="180" alt="" />
+            <a href="#" class="text-nowrap logo-img">
+                <img src="{{ asset('dash/assets/images/logos/logo-humas.png') }}" width="180" alt="" />
             </a>
             <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                 <i class="ti ti-x fs-8"></i>
